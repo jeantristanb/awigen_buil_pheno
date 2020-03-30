6ede8cb5b0e04e1a8bd25331053c6712f905cde4
jeantristanb <jeantristanb@free.fr>
2020-03-30 17:09:10 +0200

    update build pheno

--- launchAnalyse_all.r
+++ launchAnalyse_all.r
@@ -1,157 +1,198 @@
+#!/usr/bin/Rscript
 library('knitr')
 library("optparse")
 library('kableExtra')
 library(VennDiagram)
-source("script/fct.r")
+
+dirscript=dirname(commandArgs()[4])
+source(paste(strsplit(dirscript,split="=")[[1]][2],"script/fct.r",sep='/'))
 
 option_list = list(
-  make_option(c("--pheno_file"), type="character", default=NULL, 
+  make_option("--pheno_file", type="character", default=NULL, 
               help="initial file of phenotype", metavar="character"),
-  make_option(c("--sep"), type="character", default=NULL, 
+  make_option("--sep", type="character", default=NULL, 
               help="separator for pheno file", metavar="character"),
-  make_option(c("--head_fid"), type="character", default='FID',
+  make_option("--head_id", type="character", default='FID,IID',
               help="head fid", metavar="character"),
-  make_option(c("--param"), type="character", default=NULL, 
+  make_option("--params", type="character", default=NULL, 
               help="separator for pheno file", metavar="character"),
-  make_option(c("--pheno"), type="character", default=NULL, 
+  make_option("--tr_var", type="character", default=NULL, 
+              help="separator for pheno file", metavar="character"),
+  make_option("--pheno", type="character", default=NULL, 
               help="initial file of phenotype", metavar="character"),
-  make_option(c("--var"), type="character", default=NULL, 
+  make_option("--var", type="character", default=NULL, 
               help="initial file of phenotype", metavar="character"),
-  make_option(c("--tr"), type="character", default=NULL, 
+  make_option("--tr", type="character", default=NULL, 
               help="tranformation need", metavar="character"),
-  make_option(c("--id_keep"), type="character", default=NULL, 
+  make_option("--fam_file", type="character", default=NULL, 
               help="file id to keep", metavar="character"),
-  make_option(c("--dir_out"), type="character", default="out.txt", 
+  make_option("--dir_out", type="character", default="./", 
+              help="dir out file [default= %default]", metavar="character"),
+  make_option("--output", type="character", default="out.txt", 
               help="output file name [default= %default]", metavar="character")
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
-opt = parse_args(opt_parser);
-
-#args = commandArgs(trailingOnly=TRUE)
+args = parse_args(opt_parser);
+listargcheck=c('dir_out', 'sep', 'pheno_file', 'head_id', 'params', 'fam_file')
+for(arg in listargcheck){
+if(is.null(args[[arg]])){
+cat('not found ',arg, '\n exit\n')
+q('no',2)
+}
+}
 #if(length(args)==0){
 #Var="meancimtleft"
 #DirOut="/spaces/jeantristan/GWAS/Romuald/GWAS/buildpheno/"
-#}else{
+#}elseargs{
 #Var<-args[1]
 #DirOut<-args[2]
 #}
-dir_out=opt[['dir_out']]
-file_phenoI=opt[['pheno_file']]
-sep=
+dir_out=args[['dir_out']]
+file_phenoI=args[['pheno_file']]
+sep=GetSep(args[['sep']])
+if(is.na(sep)){
+cat('\n','not found sep', args[['sep']],'\n')
+q('no',2)
+}
 if(!is.null(args[['params']])){
 data_param=read.table(args[['params']] ,header=T)
 }else{
-Var=opt[['var']]
-q(status = 2)
+Var=args[['var']]
+q('no', 2)
 }
 
-dir.create(DirOut)
+dir.create(dir_out)
 ## PCA file
 ### b
 ## 
-DirOutF=paste(DirOut,"/report_pdf",sep="")
+DirOutF=paste(dir_out,"/report_pdf",sep="")
 if(!dir.exists(DirOutF))dir.create(DirOutF)
 
-#fileouput=paste(DirOut,"/DataRes_cIMT_GWAS_03APR2019.csv",sep="")
+fileouput=paste(dir_out,"/", args[['output']],sep="")
+DataI<-read.csv(file_phenoI, sep=sep)
 
-DataI<-read.csv(FileI)
-DataI$site_qc<-c("Agincourt", "Dikgale", "Nairobi","Nanoro","Navrongo", "SOWETO")[DataI$site]
-DataI$sex_qc<-DataI$sex
-DataI$sex<-NA
-DataI$sex[DataI$sex_qc=="Women"]<-0
-DataI$sex[DataI$sex_qc=="Men"]<-1
-
-names(DataI)[1:2]<-c("FID","IID")
-listepca=list()
-listenpca=list()
-
-DirPca="/dataE/AWIGenGWAS/shared/ResultGWAS/Ressource/Pca/prune_50_10_0.1/"
-ListeGroupePca=c("All","Females", "Males", "South", "West", "East")
-
-for(HeadPca in ListeGroupePca) {
-listepca[[HeadPca]]=paste(DirPca,HeadPca,".eigenvec",sep="")
-#listenpca=list(All=8, East=2, West=4, South=3, Women=5, Men=5)
-if(length(grep("All", HeadPca))>0)listenpca[[HeadPca]]=8
-else if(length(grep("East", HeadPca))>0)listenpca[[HeadPca]]=2
-else if(length(grep("West", HeadPca))>0)listenpca[[HeadPca]]=4
-else if(length(grep("South", HeadPca))>0)listenpca[[HeadPca]]=3
-else if(length(grep("Women", HeadPca))>0)listenpca[[HeadPca]]=5
-else if(length(grep("Females", HeadPca))>0)listenpca[[HeadPca]]=5
-else if(length(grep("Men", HeadPca))>0)listenpca[[HeadPca]]=5
-else if(length(grep("Males", HeadPca))>0)listenpca[[HeadPca]]=5
-else {
-cat("not found ", HeadPca, "for pca number")
-q()
+## must be 2
+fidiid=strsplit(args[['head_id']] ,split=',')[[1]]
+if(length(fidiid)!=2 | any(!(fidiid %in% names(DataI)))){
+cat('\n','problem --head_id  not found', args[['head_id']], '\n')
+q('no',2)
 }
-if(!file.exists(listepca[[HeadPca]])){
-cat("Don't found ", listepca[[HeadPca]])
-q()
-}
+names(DataI)[fidiid[1]==names(DataI)]<-'FID'
+names(DataI)[fidiid[2]==names(DataI)]<-'IID'
+if(!is.null(args[['tr_var']])){
+cat('\ntransformation of variables\n')
+tr_var=read.table(args[['tr_var']],header=T, stringsAsFactors=F,sep=' ')
+DataI<-transform_allvar(DataI, tr_var)
 }
 
-DataLim<-read.table("corrvaluelimckd.in", header=T)
-infovariable=""
+DataFam<-read.table(args[['fam_file']])
+for(CmtVar in 1:nrow(data_param)){
+## pca
+tmp<-getinfopca(DataFam[CmtVar,])
+listepca=tmp[['pca']]
+listenpca=tmp[['npca']]
+## 
+UpperLimit=
+
+}
+##
+
+#DataI$site_qc<-c("Agincourt", "Dikgale", "Nairobi","Nanoro","Navrongo", "SOWETO")[DataI$site]
+#DataI$sex_qc<-DataI$sex
+#DataI$sex<-NA
+#DataI$sex[DataI$sex_qc=="Women"]<-0
+#DataI$sex[DataI$sex_qc=="Men"]<-1
+
+
+
+#listepca=list()
+#listenpca=list()
+
+#DirPca="/dataE/AWIGenGWAS/shared/ResultGWAS/Ressource/Pca/prune_50_10_0.1/"
+#ListeGroupePca=c("All","Females", "Males", "South", "West", "East")
+#for(HeadPca in ListeGroupePca) {
+#listepca[[HeadPca]]=paste(DirPca,HeadPca,".eigenvec",sep="")
+##listenpca=list(All=8, East=2, West=4, South=3, Women=5, Men=5)
+#if(length(grep("All", HeadPca))>0)listenpca[[HeadPca]]=8
+#else if(length(grep("East", HeadPca))>0)listenpca[[HeadPca]]=2
+#else if(length(grep("West", HeadPca))>0)listenpca[[HeadPca]]=4
+#else if(length(grep("South", HeadPca))>0)listenpca[[HeadPca]]=3
+#else if(length(grep("Women", HeadPca))>0)listenpca[[HeadPca]]=5
+#else if(length(grep("Females", HeadPca))>0)listenpca[[HeadPca]]=5
+#else if(length(grep("Men", HeadPca))>0)listenpca[[HeadPca]]=5
+#else if(length(grep("Males", HeadPca))>0)listenpca[[HeadPca]]=5
+#else {
+#cat("not found ", HeadPca, "for pca number")
+#q()
+#}
+#if(!file.exists(listepca[[HeadPca]])){
+#cat("Don't found ", listepca[[HeadPca]])
+#q()
+#}
+#}
+
+#DataLim<-read.table("corrvaluelimckd.in", header=T)
+#infovariable=""
 
 #listenpca=list(All=6, East=4, West=4, South=4)
-FamAll="/dataE/AWIGenGWAS/plink/awigen/qc/awigen-qc.fam"
-DataPcaAll<-read.table(FamAll)
+#DataPcaAll<-read.table(FamAll)
 
 #Var<-"friedewald_qc"
-DeletedSweet=F
-VarSite="site_qc"
-VarSex<-"sex"
-VarId="FID"
-VarSweetCtr<-"cohort_id_c";VarSweet<-"SWEET"
-Soweto<-"SOWETO"
-listM<-list()
-ValSexF=0
-M111<-NA
-M222<-NA
-NbSdLim<-NA
+#DeletedSweet=F
+#VarSite="site_qc"
+#VarSex<-"sex"
+#VarId="FID"
+#VarSweetCtr<-"cohort_id_c";VarSweet<-"SWEET"
+#Soweto<-"SOWETO"
+#listM<-list()
+#ValSexF=0
+#M111<-NA
+#M222<-NA
+#NbSdLim<-NA
 # to delete
-listcovF<-c("age","sex", "bmi_qc")
-Listlistcov<-list(agesexpca=c("age","sex"))
-if(Var %in% c("waist_hip_r_c_qc", "visceral_fat_qc")){
-Listlistcov[["agesexbmipca"]]=c("age","sex", "bmi_qc")
-}
+#listcovF<-c("age","sex", "bmi_qc")
+#Listlistcov<-list(agesexpca=c("age","sex"))
+#if(Var %in% c("waist_hip_r_c_qc", "visceral_fat_qc")){
+#Listlistcov[["agesexbmipca"]]=c("age","sex", "bmi_qc")
+#}
 
 #fcntr<-RankNormTr
 #namtr<-"Rank-based inverse normal transformations"
 #namtrshort<-"rin"
 
-fcntr<-function(x)x
-namtr<-"No transformation"
-namtrshort<-"notr"
+#fcntr<-function(x)x
+#namtr<-"No transformation"
+#namtrshort<-"notr"
 
-allcovusedused<-c(listcovF, "hiv_final_status_c")
+#allcovusedused<-c(listcovF, "hiv_final_status_c")
 #Data<-DataI[DataI[,VarId] %in% DataPcaAll[,1] ,]
-Data<-DataI
+#Data<-DataI
 #https://www.ncbi.nlm.nih.gov/pubmed/22898763
 
 
-if(Var %in% DataLim[,1]){
-M111<-DataLim[DataLim[,1]==Var,2]
-M222<-DataLim[DataLim[,1]==Var,3]
-DeletedSweet=DataLim[DataLim[,1]==Var,4]
-if(DataLim[DataLim[,1]==Var,4]==F)listcovF<-c("age","sex")
-NbSdLim=DataLim[DataLim[,1]==Var,6]
-}
-Females<-0
-## transform hiv
-Data$hiv_final_status_c[Data$hiv_final_status_c==2 & Data$site_qc %in% c("Nanoro","Navrongo")]<-0
-Data$age2<-Data$age**2
-Data$hiv_final_status_c[Data$hiv_final_status_c==2]<-NA
-Data$diabetes_status_c_qc[Data$diabetes_status_c_qc<0]<-NA
-Data$htn_jnc7_qc[Data$htn_jnc7_qc<0]<-NA
-
-
-
+#if(Var %in% DataLim[,1]){
+#M111<-DataLim[DataLim[,1]==Var,2]
+#M222<-DataLim[DataLim[,1]==Var,3]
+#DeletedSweet=DataLim[DataLim[,1]==Var,4]
+#if(DataLim[DataLim[,1]==Var,4]==F)listcovF<-c("age","sex")
+#NbSdLim=DataLim[DataLim[,1]==Var,6]
+#}
+#Females<-0
+### transform hiv
+#Data$hiv_final_status_c[Data$hiv_final_status_c==2 & Data$site_qc %in% c("Nanoro","Navrongo")]<-0
+#Data$age2<-Data$age**2
+#Data$hiv_final_status_c[Data$hiv_final_status_c==2]<-NA
+#Data$diabetes_status_c_qc[Data$diabetes_status_c_qc<0]<-NA
+#Data$htn_jnc7_qc[Data$htn_jnc7_qc<0]<-NA
+#
+#
+#
 fileouputlog<-paste(fileouput,".log",sep="")
-if(!file.exists(fileouput)){
-write.table(Data[,c(names(Data)[c(1,2)], unique(c(VarSite,VarSex, unlist(Listlistcov))))], file=fileouput, quote=F, sep="\t",row.names=F, col.names=T)
-writeLines("Var\tVarI\tCov\tNbSd\tRegion\tTrans\tNpca",con=fileouputlog)
-}
-#FileRnw=paste(DirOut,"/report/",Var, ".Rnw",sep="")
-FileRnw="/dataE/AWIGenGWAS/shared/ResultGWAS/Ressource/pheno/script/TemplateAnalyse.Rnw"
-knit(FileRnw, output=paste(DirOutF,"/",Var,".tex",sep=""))
+#if(!file.exists(fileouput)){
+#write.table(Data[,c(names(Data)[c(1,2)], unique(c(VarSite,VarSex, unlist(Listlistcov))))], file=fileouput, quote=F, sep="\t",row.names=F, col.names=T)
+#writeLines("Var\tVarI\tCov\tNbSd\tRegion\tTrans\tNpca",con=fileouputlog)
+#}
+##FileRnw=paste(DirOut,"/report/",Var, ".Rnw",sep="")
+#FileRnw="/dataE/AWIGenGWAS/shared/ResultGWAS/Ressource/pheno/script/TemplateAnalyse.Rnw"
+#knit(FileRnw, output=paste(DirOutF,"/",Var,".tex",sep=""))
---
+++ script/fct.r
@@ -0,0 +1,247 @@
+library(scales)
+getinfopca<-function(paraminfo){
+if(is.na(paraminfo$filepca))return(list('pca'=list(), 'npca'=list()))
+names(paraminfo)<-tolower(paraminfo)
+lispca<-strsplit(paraminfo$filepca,split=';')[[1]]
+lisnamepca<-strsplit(paraminfo$headpca,split=';')[[1]]
+lisnpca<-strsplit(paraminfo$npca,split=';')[[1]]
+if((length(lisnpca)!=length(lispca))!=length(lisnamepca)){
+cat('probleme in pca')
+q('no',2)
+}
+fpca=list()
+npca=list()
+for(Cmt in 1:length(lispca)){
+if(!file.exists(lispca[Cmt])){
+cat('\npca file ',lispca[Cmt], 'doesn t exist \n exit\n')
+q('no',2)
+}
+fpca[[lisnamepca[Cmt]]]=lispca[Cmt]
+npca[[lisnamepca[Cmt]]]=as.integer(lisnpca[Cmt])
+
+}
+}
+
+dotrans<-function(x, tr){
+cat('\ntransformation not done yet\n')
+q('no',2)
+}
+
+transform_allvar<-function(Data, param){
+#newvariable variable transf changein changeout LimLower LimUpper
+#iur_albumin_qc2 ur_albumin_qc NA "==-111";"==-222" 3,401 NA NA
+ names(param)<-tolower(names(param))
+ for(Cmt in 1:nrow(param)){
+   infoparam<-param[Cmt,]
+   newvar<-Data[,infoparam$variable]
+   if(!is.na(infoparam$changein)){
+    listinstchange<-strsplit(infoparam$changein,split=';')[[1]]
+    listtochange<-strsplit(infoparam$changeout,split=';')[[1]]
+    if(length(listinstchange)!=length(listtochange)){
+     print(listinstchange)
+     print(listtochange)
+     cat('\n changein and changeout different in transformation variable, variable\n')
+     q(2)
+    }
+    for(CmtChange in 1:length(listinstchange)){
+     newvar<-transformvar(newvar,listinstchange[CmtChange], listtochange[CmtChange])
+    }
+  }
+  if(!is.na(infoparam[,'limlower']))newvar[!is.na(newvar) & newvar<infoparam[,'limlower']]<-NA
+  if(!is.na(infoparam[,'limupper']))newvar[!is.na(newvar) & newvar>infoparam[,'limupper']]<-NA
+  if(!is.na(infoparam[,'transf']))newvar<-dotrans(newvar, infoparam[,'transf'])
+  Data[,infoparam$newvariable]<-newvar
+ }
+ return(Data)
+}
+
+transformvar<-function(vect, expr, new){
+if(is.na(expr))return(vect)
+typevec<-typeof(vect)
+if(!is.na(new) & toupper(new)=="NA")new=NA
+else{
+#if(any(is.character(vect)) | is.factor(vect))new=new
+if(any(is.integer(vect)))new=as.integer(new)
+else if(any(is.double(vect)))new=as.double(new)
+}
+bal<-eval(parse(text=paste('vect',expr)))
+vect[bal]<-new
+vect
+}
+formulastr<-function(data,form){
+attach(data)
+eval(parse(text=form))
+}
+
+GetSep<-function(x){
+listsep=c(',',';', '\t', ' ')
+listspen=c('COM', 'SEM', 'TAB', 'SPA')
+if(x %in% listsep)return(x)
+x2<-substring(toupper(x),1,3)
+if(x2 %in% listspen)return(listsep[listspen==x2])
+return(NA)
+}
+GetSig<-function(lx){
+sapply(lx, function(x){
+if(is.na(x))return("")
+if(x>0.1)return("")
+if(x>0.05)return(".")
+if(x>0.01)return("*")
+if(x>0.001)return("**")
+return("***")
+})
+}
+
+as.characterspe<-function(x,round){
+y<-rep(NA, length(x))
+bal=!is.na(x) & x>10**-round
+y[bal]<-as.character(format(round(x[bal],round), scientific=F))
+y[!bal]<-as.character(format(x[!bal], scientific=T,digits=round))
+y
+}
+
+Dund<-function(x,rep=" ")gsub("_",rep,x)
+plotBySite<-function(Data,Var,site, xlab="value", ylab="density", main=""){
+        Data<-na.omit(Data[,c(Var,site)])
+        ListSit<-unique(Data[,site])
+        denslist<-lapply(1:length(ListSit), function(x)density(Data[Data[,site]==ListSit[x],Var]))
+        xlim<-range(sapply(1:length(denslist),function(x)return(range(denslist[[x]]$x))))
+        ylim<-range(sapply(1:length(denslist),function(x)return(range(denslist[[x]]$y))))
+        plot(xlim, ylim,type="n", xlab=xlab,ylab=ylab, main=main)
+        Cmt<-1
+        for(x in denslist){
+                        lines(x,col=Cmt)
+                Cmt<-Cmt+1
+        }
+        legend("topright", legend=ListSit, lty=1, col=1:Cmt,bty='n')
+}
+
+
+plotBySiteSex<-function(Data,Var,site, sex, InfoSite, InfoSex,xlab="value", ylab="density", main=""){
+        Data<-na.omit(Data[,c(Var,site,sex)])
+        ListSit<-unique(Data[,c(site,sex)])
+        ListSit<-merge(merge(ListSit,InfoSite),InfoSex)
+        denslist<-lapply(1:nrow(ListSit), function(x)density(Data[Data[,site]==ListSit[x,2] & Data[,sex]==ListSit[x,1],Var]))
+        xlim<-range(sapply(1:length(denslist),function(x)return(range(denslist[[x]]$x))))
+        ylim<-range(sapply(1:length(denslist),function(x)return(range(denslist[[x]]$y))))
+        plot(xlim, ylim,type="n",xlab=xlab,ylab=ylab, main=main)
+        Cmt<-1
+        for(x in denslist){
+                        lines(x,col=ListSit[Cmt,"ColSite"], lty=ListSit[Cmt,"ColSex"])
+                Cmt<-Cmt+1
+        }
+      InfoSexSpe<-unique(ListSit[,c(sex,"ColSex")])
+      InfoSiteSpe<-unique(ListSit[,c(site,"ColSite")])
+      legend("topleft", legend=c(as.character(InfoSexSpe[,VarSex]), as.character(InfoSiteSpe[,VarSite])), col=c(rep(1,length(InfoSexSpe[,VarSex])), InfoSiteSpe[,"ColSite"]), lty=c(InfoSexSpe[,"ColSex"], rep(1,length(InfoSiteSpe[,"ColSite"]))),bty="n")
+}
+
+
+resumestat<-function(x){
+nmiss<-length(x[is.na(x)])
+x<-x[!is.na(x)]
+res<-c(nmiss,length(x),mean(x), median(x), sd(x),min(x),max(x))
+names(res)<-c("nmiss","n","mean","med","sd","min","max")
+return(res)
+}
+GetDistMiss<-function(x){
+res<-c(GetPerc(x==-555), GetPerc(x==-999),GetPerc(x==-111) ,GetPerc(x==-222),GetPerc(x>-111 & x<0), GetPerc(x==0), GetPerc(is.na(x)), GetPerc(x>0))
+names(res)<-c("-555","-999","-111","-222","<0","==0","NA",">0")
+return(res)
+}
+GetPerc<-function(x)length(which(x))/length(x)*100
+
+PlotPca<-function(Data, Site, listcovF, listepca, listenpca){
+DataPca<-read.table(listepca[[Site]])
+npca<-listenpca[[Site]]
+names(DataPca)<-c("FID","IID",paste("Axis_",1:(ncol(DataPca)-2),sep=""))
+
+DataAndPca<-merge(Data,DataPca,by.x=VarId,by.y="FID")
+DataAndPca[,VarSite]<-as.character(DataAndPca[,VarSite])
+rownames(DataAndPca)<-DataAndPca[,VarId]
+resglm<-glm(as.formula(paste(Var,"~",paste(c(listcovF, paste("Axis_",1:npca,sep="")),collapse="+"),sep="")), data=DataAndPca)
+sumresglm<-summary(resglm)
+print(kable(sumresglm$coefficients, row.names=T, booktabs = T, caption=paste("summary for glm with pca"),format = "latex")  %>% kable_styling(latex_options = c("striped", "HOLD_position")))
+DataAndPca<-merge(DataAndPca,data.frame(Pos=names(resglm$residuals),res_agesexbmipca=resglm$residuals-min(resglm$residuals)+1),by.x=VarId, by.y=1)
+boxplot(as.formula(paste("res_agesexbmipca~",VarSex,"+",VarSite)), data=DataAndPca, las=3)
+
+#sizepch=(DataAndPca[,Var]-min(DataAndPca[,Var],na.rm=T))/(max(DataAndPca[,Var])-min(DataAndPca[,Var]))*3+1
+DataAndPca<-DataAndPca[!is.na(DataAndPca[,Var]),]
+sizepch=(DataAndPca[,Var]-mean(DataAndPca[,Var], na.rm=T))/sd(DataAndPca[,Var],na.rm=T)
+sizepch=sizepch-min(sizepch)
+sizepch<-order(DataAndPca[,Var])/nrow(DataAndPca)*2.5
+Col<-unique(DataAndPca[,c("site","site_qc")])
+plot(DataAndPca$Axis_1, DataAndPca$Axis_2,col=alpha(DataAndPca$site,0.4),pch=DataAndPca$sex+15,cex=sizepch,xlab="axis1", ylab="axis2")
+legend("topright",legend=Col[,"site_qc"], col=Col[,"site"], pch=1, bty='n')
+plot(DataAndPca$Axis_3, DataAndPca$Axis_4,col=alpha(DataAndPca$site,0.4),pch=DataAndPca$sex+15,cex=sizepch,xlab="axis4", ylab="axis4")
+legend("topright",legend=Col[,"site_qc"], col=Col[,"site"], pch=1, bty='n')
+
+return(DataAndPca)
+}
+
+PlotPca2<-function(Data, Region, listepca, listenpca, Var,VarSite, VarSex,VarId,InfoSite, InfoSex){
+DataPca<-read.table(listepca[[Region]])
+DataEig<-read.table(gsub("eigenvec","eigenval",listepca[[Region]]))
+npca<-listenpca[[Region]]
+names(DataPca)<-c("FID","IID",paste("Axis_",1:(ncol(DataPca)-2),sep=""))
+DataAndPca<-merge(Data,DataPca,by.x=VarId,by.y="FID")
+DataAndPca[,VarSite]<-as.character(DataAndPca[,VarSite])
+rownames(DataAndPca)<-DataAndPca[,VarId]
+DataAndPca<-DataAndPca[!is.na(DataAndPca[,Var]),]
+sizepch=(DataAndPca[,Var]-mean(DataAndPca[,Var], na.rm=T))/sd(DataAndPca[,Var],na.rm=T)
+sizepch=sizepch-min(sizepch)
+sizepch<-order(DataAndPca[,Var])/nrow(DataAndPca)*2.5
+DataAndPca<-merge(merge(DataAndPca,InfoSite,by=VarSite,all.x=T), InfoSex, by=VarSex,all.x=T)
+par(mfrow=c(2,2))
+plot(DataEig[,1], xlab="eigen num", ylab="variance explained",main="")
+plot(DataAndPca$Axis_1, DataAndPca$Axis_2,col=alpha(DataAndPca$ColSite,0.2),pch=DataAndPca$ColSex+15,cex=sizepch,xlab="axis1", ylab="axis2")
+legend("topright",legend=InfoSite[,VarSite], col=InfoSite[,"ColSite"], pch=1, bty='n')
+plot(DataAndPca$Axis_3, DataAndPca$Axis_4,col=alpha(DataAndPca$ColSite,0.2),pch=DataAndPca$ColSex+15,cex=sizepch,xlab="axis3", ylab="axis4")
+plot(DataAndPca$Axis_5, DataAndPca$Axis_6,col=alpha(DataAndPca$ColSite,0.2),pch=DataAndPca$ColSex+15,cex=sizepch,xlab="axis5", ylab="axis6")
+return(DataAndPca)
+}
+
+
+
+RankNormTr<-function(a)qnorm((rank(a)-0.5)/(length(a)-2*0.5+1))
+
+GetLinearisation<-function(Site, listWithPca){
+valueI<-function(x)x
+squareroot<-function(x)x**2
+cuberoot<-function(x){
+y<-x**3
+y[x<0]<- -y[x<0]
+y
+}
+
+xval<-listWithPca[[Site]][,"res_agesexbmipca"]
+meanval<-mean(xval, na.rm=T)
+valsddata<-sd(xval,na.rm=T)*3:6
+plot(density(xval, na.rm=T), main="density residuals with pca")
+abline(v=meanval+valsddata,col=2:(length(valsddata)+1))
+abline(v=meanval-valsddata,col=2:(length(valsddata)+1))
+#
+listvalsd<-c()
+for(sdval in valsddata){
+listvalsd<-c(listvalsd, length(xval[xval>meanval+sdval|xval< meanval-sdval]))
+}
+listvalsd<-matrix(listvalsd, nrow=1)
+colnames(listvalsd)<-paste(3:6,"sd")
+print(kable(listvalsd, caption=c("values higher than x * sd"), row.names=F, booktabs = T, format = "latex")  %>% kable_styling(latex_options = c("striped", "HOLD_position")))
+
+par(mfrow=c(2,2))
+listefunction<-c("valueI", "log", "squareroot", "cuberoot")
+Cmt<-1
+for(trans in listefunction){
+xvaltr<-get(trans)(xval)
+plot(density(xvaltr, na.rm=T), main=trans)
+kstrs<-ks.test(xvaltr, "pnorm", mean(xvaltr,na.rm=T), sd(xvaltr,na.rm=T))
+ksres<-c(kstrs$statistic,kstrs$p.value)
+if(Cmt==1)ksresall<-ksres
+else ksresall<-rbind(ksresall,ksres)
+Cmt<-Cmt+1
+}
+colnames(ksresall)<-c("D","p.value")
+rownames(ksresall)<-listefunction
+kable(cbind(Transformation=listefunction,as.data.frame(ksresall)), row.names=T, booktabs = T, caption=paste("ks test for various transformation on data"),format = "latex")  %>% kable_styling(latex_options = c("striped", "HOLD_position"))
+}
+
